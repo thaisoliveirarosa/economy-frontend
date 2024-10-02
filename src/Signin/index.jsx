e19aee8bb0a3655c3e358b22a00b9f6f1370ef1c
Thais Oliveira Rosa <thaisoliveiraerosa@gmail.com>
2024-10-01 23:39:42 -0300

    Criando cadastro e login

--- src/Signin/index.jsx
+++ src/Signin/index.jsx
@@ -1,5 +1,64 @@
+import { useEffect, useState } from 'react';
 import './style.css'
+import axios from 'axios';
 
 export default function Signin() {
-  return <>Entrar</>
+
+  const [formData, setFormData] = useState({
+    email: '',
+    senha: ''
+  });
+
+  function handleChange(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newObject = {
+      email: formData.email,
+      senha: formData.senha
+    };
+
+    axios.post('http://localhost:8080/user/login', newObject)
+      .then((response) => {
+        console.log('Object created successfully:', response.data);
+        localStorage.setItem("token", response.data.token);
+        window.location.href = "/";
+      })
+      .catch((error) => {
+        console.error('There was an error creating the object:', error);
+      });
+  };
+
+  useEffect(() => {
+    console.log("TOKEN: ", localStorage.getItem("token"));
+  }, [])
+
+  return <form onSubmit={handleSubmit}>
+    <div>
+      <label>E-email</label>
+      <input
+        type="text"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+        required
+      />
+    </div>
+    <div>
+      <label>Senha</label>
+      <input
+        type="password"
+        name="senha"
+        value={formData.senha}
+        onChange={handleChange}
+        required
+      />
+    </div>
+    <button type="submit" className="submit-button">Logar</button>
+  </form>
 }
--- src/Signup/index.jsx
+++ src/Signup/index.jsx
@@ -1,5 +1,70 @@
+import { useState } from 'react';
 import './style.css'
+import axios from 'axios';
 
 export default function Signup() {
-  return <>Cadastrar</>
+
+  const [formData, setFormData] = useState({
+    nome: '',
+    email: '',
+    senha: ''
+  });
+
+  function handleChange(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newObject = {
+      nome: formData.nome,
+      email: formData.email,
+      senha: formData.senha
+    };
+
+    axios.post('http://localhost:8080/user/create', newObject)
+      .then((response) => {
+        console.log('Object created successfully:', response.data);
+      })
+      .catch((error) => {
+        console.error('There was an error creating the object:', error);
+      });
+  };
+
+  return <form onSubmit={handleSubmit}>
+    <div>
+      <label>Nome</label>
+      <input
+        type="text"
+        name="nome"
+        value={formData.nome}
+        onChange={handleChange}
+        required
+      />
+    </div>
+    <div>
+      <label>E-email</label>
+      <input
+        type="text"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+        required
+      />
+    </div>
+    <div>
+      <label>Senha</label>
+      <input
+        type="password"
+        name="senha"
+        value={formData.senha}
+        onChange={handleChange}
+        required
+      />
+    </div>
+    <button type="submit" className="submit-button">Criar Usuario</button>
+  </form>
 }
